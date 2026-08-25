a157b212e47e1ae7f7ba0117bd3e4a88156080a1
Gustavo Lima <limagustavo2200@gmail.com>
2026-08-25 14:03:28 -0300

    fallback para admin no GET /ticket quando security está desabilitado

--- src/main/java/com/gustavo/helpdesk_api/infra/controller/ticket/TicketController.java
+++ src/main/java/com/gustavo/helpdesk_api/infra/controller/ticket/TicketController.java
@@ -16,6 +16,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -66,7 +67,10 @@ public class TicketController {
    public ResponseEntity<List<TicketOutputDto>> findAll(
       @RequestParam(required = false) List<UUID> statusIds
    ) {
-      JwtPayload jwt = (JwtPayload) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      JwtPayload jwt = (authentication != null)
+         ? (JwtPayload) authentication.getPrincipal()
+         : new JwtPayload(null, null, null, "admin");
       var tickets = findAllTicketsUseCase.execute(statusIds, jwt);
       return ResponseEntity.status(HttpStatus.OK).body(tickets);
    }
